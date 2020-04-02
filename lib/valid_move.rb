920ed60f17269df5546f7a7997c83e6c6293a02b
Val Osip <valosip@gmail.com>
2020-04-02 21:38:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,11 @@
 
 def position_taken(board, index)
 
-  if board[index] == "" || 
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  else 
+    true
+  end
 
 end
 
