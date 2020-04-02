c963a9498131954ca2c0b3c5fd0c4cbfce915060
Val Osip <valosip@gmail.com>
2020-04-02 21:48:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,7 +18,7 @@ def valid_move?(board, index)
   if !index.between?(0,8) 
     return false
   end
-  if !position_taken(board, index)
+  if !position_taken?(board, index)
     true
   else 
     false
